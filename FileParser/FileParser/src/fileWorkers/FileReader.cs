b0bcc180a6e3e2d4c484c7225e3d8c9a133d7501
Timur Gulyaev <jokerlg990nbvrf@gmail.com>
2022-12-09 00:04:28 +0700

    FileParser

---
+++ FileParser/FileParser/src/fileWorkers/FileReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileParser.src.contexts;
+using FileParser.src.exceptions;
+using Microsoft.SqlServer.Server;
+
+namespace FileParser.src.fileWorkers
+{
+    public class FileReader 
+    {
+        private readonly StreamReader streamReader;
+
+        private readonly StringParser stringParser;
+        public FileReader(string fileName, ParsedFileContext parsedFileContext) 
+        {
+            try
+            {
+                streamReader = new StreamReader(fileName);
+            }catch (IOException e)
+            {
+                throw new BadInputFileNameException("Bad file name... Try again:\n" + e.Message);
+            }
+
+            stringParser = new StringParser(parsedFileContext);
+        }
+
+        public void ReadFile() 
+        {   
+            
+            string line;
+
+            line = streamReader.ReadLine();
+
+            while (line != null)
+            {
+                stringParser.ParseString(line + " ");
+                line = streamReader.ReadLine();
+            }
+
+        }
+
+    }
+}
